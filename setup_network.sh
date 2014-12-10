7ae6a964d53211184d3829f652d5c04cbc1914c3
Maxwell Dergosits <mdergosits@gmail.com>
2014-12-10 14:06:10 -0500

    udp script

---
+++ setup_network.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sysctl -w net.core.rmem_max=8388608
+sysctl -w net.core.wmem_max=8388608
+sysctl -w net.core.rmem_default=65536
+sysctl -w net.core.wmem_default=65536
+sysctl -w net.ipv4.udp_rmem_min=65536
+sysctl -w net.ipv4.udp_wmem_min=65536
+sysctl -w net.ipv4.udp_mem='8388608 8388608 8388608'
+sysctl -w net.ipv4.route.flush=1
